e0821c8a0cf552e39b2ae484a1a7c9942adb6202
SWETA KALASAGONDA <sweta.kalasagonda@kyndryl.com>
2022-02-05 16:05:24 +0530

    My first checkin

---
+++ AirthmeticClassDemo.java
@@ -0,0 +1,29 @@
+package testpckg;
+
+public class AirthmeticClassDemo {
+	public static void main(String[] args) {
+		
+		//Addition
+		int A = 10 + 20;
+		System.out.println("Addition result is :" +A);
+		
+		//Subtraction
+		A = A - 5;
+		System.out.println("Subtraction result is :" +A);	
+		
+		//Division
+		A = A / 3;
+		System.out.println("Division result is :" +A);
+		
+		//Multiplication
+		A = A * 2;
+		System.out.println("Multiplication result is :" +A);
+		
+		//Modulos
+		A = A % 3;
+		System.out.println("Modulous result is :" +A);
+				
+				
+	}
+
+}
---
+++ TestHelloWorld.java
@@ -0,0 +1,10 @@
+package testpckg;
+
+public class TestHelloWorld {
+	public static void main(String arg[]) {
+		System.out.println("Hello World!");
+		System.out.println("Hello India!");
+		
+	}
+
+}
